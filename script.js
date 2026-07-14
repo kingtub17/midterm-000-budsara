00d2c676111dd5b81296fd3fc0c50895c606aa66
budsara <kingtub17@gmail.com>
2026-07-14 23:59:41 +0700

    commit 4 : สร้าง JS

---
+++ script.js
@@ -0,0 +1,4 @@
+// ดักจับเหตุการณ์เมื่อผู้ใช้คลิกปุ่มที่มี id เป็น alertBtn
+document.getElementById('alertBtn').addEventListener('click', function() {
+    alert('ยินดีต้อนรับสู่คลังอนิเมะสุดรักของฉัน! ขอบคุณที่คุณแวะมาดูนะครูกิ่ง');
+});
